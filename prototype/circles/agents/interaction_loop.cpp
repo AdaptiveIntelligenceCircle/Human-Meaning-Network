48991c651aba43629aae616cd70d5f54c2bb1025
ndtribk <ndtribk@gmail.com>
2026-01-29 10:40:22 +0700

    interaction_loop.cpp

--- prototype/circles/agents/interaction_loop.cpp
+++ prototype/circles/agents/interaction_loop.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 namespace hmn :: prototype :: circles :: agents 
 {
-    
+    CircleAgent ::CircleAgent(const string &id)
+    {
+        circle_id = id; 
+    }
+
+    void CircleAgent :: observe_message(const string &)
+    {
+        message_count++; 
+    }
+
+    bool CircleAgent :: should_pause() const 
+    {
+        // qua nhieu tuong tac -> can cham lai..
+        return message_count > 30; 
+    }
 }
