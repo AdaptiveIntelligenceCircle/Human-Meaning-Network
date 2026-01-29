a48df3032dfcdf79b0403ae9560b8c18d7ec32b4
ndtribk <ndtribk@gmail.com>
2026-01-29 12:19:03 +0700

    introspection_policy.h

--- prototype/journaling/introspective/introspection_entry.h
+++ prototype/journaling/introspective/introspection_entry.h
@@ -1,13 +1,61 @@
 #pragma once 
 #include <iostream> 
+#include <optional>
 
+#include <vector> 
 #include <string> 
 using namespace std;
 
-namespace hmn :: prototype :: jounarling :: introspective 
+namespace hmn :: jounarling 
 {
-    struct IntrospectionPolicy 
+    using timestamp = uint64_t; 
+    using EntityID = string; 
+    
+    struct ContextSnapshot 
     {
-        
+        string environment; 
+        string situation; 
+    }; 
+
+    struct ActionSummary 
+    {
+        string description; 
+        string outcome; 
+    }; 
+
+    struct ReasoningSummary 
+    {
+        // abstracted, post - processed reasoning.
+        vector<string> considerations; 
+    }; 
+
+    struct ConflictTag 
+    {
+        string a; 
+        string b; 
+    };
+
+    struct ValueTag 
+    {
+        string name; // autonomy , safety
+    };
+
+    struct IntrospectionEntry
+    {
+        timestamp time; 
+        EntityID subject; 
+
+        ContextSnapshot context;
+        ActionSummary action; 
+        ReasoningSummary reasoning; 
+
+        vector<ValueTag> values_involved; 
+        vector<ConflictTag> conflicts_detected; 
+
+        double moral_tension_score; // 0.0- 1.0
+        double external_pressure_score; // 0.0 - 1.0
+
+        optional<string> human_reference; 
     }; 
 }
+
