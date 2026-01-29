163df4fb93b0d78ec893bbcd8c772853bc51c662
ndtribk <ndtribk@gmail.com>
2026-01-29 13:04:28 +0700

    introspection_engine.cpp

---
+++ prototype/journaling/introspective/introspection_engine.cpp
@@ -0,0 +1,14 @@
+#include "introspection_entry.h"
+#include "pressure_signal.h"
+
+using namespace std; 
+#include <vector> 
+
+namespace hmn :: journaling 
+{
+    class IntrospectionEngine
+    {
+        public: 
+        
+    };
+}
