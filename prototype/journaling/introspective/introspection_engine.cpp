44d9b6076ff7d68fa43150263a1276961eb9683e
ndtribk <ndtribk@gmail.com>
2026-01-29 15:43:09 +0700

    introspection_engine.cpp

--- prototype/journaling/introspective/introspection_engine.cpp
+++ prototype/journaling/introspective/introspection_engine.cpp
@@ -9,6 +9,26 @@ namespace hmn :: journaling
     class IntrospectionEngine
     {
         public: 
-        
+        void submit(const IntrospectionEntry &entry)
+        {
+            entries_.push_back(entry); 
+        }
+
+        bool requires_human_attention() const 
+        {
+            int high_tension = 0; 
+            for (const auto &e : entries_)
+            {
+                if (e.moral_tension_score > 0.7 
+                or e.external_pressure_score > 0.7)
+                {
+                    high_tension++; 
+                }
+            }
+            return high_tension >=3; 
+        }
+
+        private: 
+        vector<IntrospectionEntry> entries_; 
     };
 }
