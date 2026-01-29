412aa41b8b2710b241618dc56879ebba7711ca6d
ndtribk <ndtribk@gmail.com>
2026-01-29 11:31:12 +0700

    bias_trace.cpp

---
+++ prototype/journaling/introspective/introspection_entry.h
@@ -0,0 +1,13 @@
+#pragma once 
+#include <iostream> 
+
+#include <string> 
+using namespace std;
+
+namespace hmn :: prototype :: jounarling :: introspective 
+{
+    struct IntrospectionPolicy 
+    {
+        
+    }; 
+}
